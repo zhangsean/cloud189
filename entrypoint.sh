fba26a8814342123c150e84f91283bd5d69b57ac
ZhangSean <zxf2342@qq.com>
2023-02-02 18:17:21 +0800

    优化docker启动脚本

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,22 @@
 set -e
 set -o pipefail
 
-if [ -n "$CLOUD189_USERNAME" -a -n "$CLOUD189_PASSWORD" ]; then
-    cloud189 login -username="$CLOUD189_USERNAME" -password="$CLOUD189_PASSWORD"
-fi
+if [ ! -e "cloud189_config.json" ]; then
+    if [ -n "$CLOUD189_USERNAME" -a -n "$CLOUD189_PASSWORD" ]; then
+        cloud189 login -username="$CLOUD189_USERNAME" -password="$CLOUD189_PASSWORD"
+    fi
 
-if [ -n "$CLOUD189_SAVEDIR" ]; then
-    cloud189 config set -savedir "$CLOUD189_SAVEDIR"
-fi
+    if [ -n "$CLOUD189_SAVEDIR" && "$CLOUD189_SAVEDIR" != "/root/Downloads" ]; then
+        cloud189 config set -savedir "$CLOUD189_SAVEDIR"
+    fi
 
-if [ -n "$CLOUD189_CACHE_SIZE" ]; then
-    cloud189 config set -cache_size "$CLOUD189_CACHE_SIZE"
-fi
+    if [ -n "$CLOUD189_CACHE_SIZE" ]; then
+        cloud189 config set -cache_size "$CLOUD189_CACHE_SIZE"
+    fi
 
-if [ -n "$CLOUD189_PROXY" ]; then
-    cloud189 config set -proxy "$CLOUD189_PROXY"
+    if [ -n "$CLOUD189_PROXY" ]; then
+        cloud189 config set -proxy "$CLOUD189_PROXY"
+    fi
 fi
 
 if [ "$1" == "sh" ]; then
